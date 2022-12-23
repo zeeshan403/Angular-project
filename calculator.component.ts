06edd6d721babea1e724e9e884bf8baaa365f53b
zeeshan403 <109273871+zeeshan403@users.noreply.github.com>
2022-12-23 19:35:34 +0500

    Add files via upload

---
+++ calculator.component.ts
@@ -0,0 +1,85 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.scss']
+})
+export class CalculatorComponent{
+
+  input:string = "";
+  result:string = "";
+  
+   pressNum(num:string){
+      //do not allow . more than onecs
+      if(num=="."){
+         if(this.input!=""){
+            const lastNum = this.getLastOperand();
+            console.log(lastNum.lastIndexOf("."))
+            if(lastNum.lastIndexOf(".")>=0) return;
+         }
+      }
+      //do not allow 0 begining
+      if(num=="0"){
+        if(this.input==""){
+           return;
+        }
+         const prevKey = this.input[this.input.length-1];
+         if(prevKey==='/'|| prevKey==='*'|| prevKey==='-'|| prevKey==='+'){
+            return;
+         }
+      }
+      this.input = this.input+num
+      this.calcAnswer();
+   }
+   getLastOperand(){
+      let pos:number;
+      console.log(this.input);
+      pos = this.input.toString().lastIndexOf("+")
+      if(this.input.toString().lastIndexOf("-")>pos)
+      pos = this.input.lastIndexOf("-")
+      if(this.input.toString().lastIndexOf("*")>pos)
+      pos = this.input.lastIndexOf("*")
+      if(this.input.toString().lastIndexOf("/")>pos)
+      pos = this.input.lastIndexOf("/")
+      console.log('Last'+this.input.substr(pos+1))
+      return this.input.substr(pos+1)
+   }
+   pressOperator(op:string){
+      //do not allow operator more than one
+      const lastKey = this.input[this.input.length-1]
+      if(lastKey==='/'|| lastKey==='*'|| lastKey==='-'|| lastKey==='+'){
+        return;
+     }
+     this.input = this.input+op
+     this.calcAnswer()
+   }
+   clear(){
+    if(this.input!=""){
+       this.input = this.input.substr(0,this.input.length-1)
+    }
+   }
+   allclear(){
+    this.input = "";
+    this.result = "";
+   }
+   calcAnswer(){
+    let formula = this.input;
+    let lastKey = formula[formula.length-1];4
+    if(lastKey==='.'){
+      formula = formula.substr(0,formula.length-1)
+    }
+    lastKey = formula[formula.length-1];
+    if(lastKey==='/'|| lastKey==='*'|| lastKey==='-'|| lastKey==='+'||lastKey==='.'){
+      formula = formula.substr(0,formula.length-1)
+   }
+   console.log("formula" +formula);
+   this.result = eval(formula);
+   }
+   getAnswer(){
+    this.calcAnswer();
+    this.input = this.result;
+    if(this.input=="0")
+    this.input = "";
+   }
+}
